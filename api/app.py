c3d2e81f4d9195e73b8a238cb88d3fa60a71f4f9
Chris Tsui <chris.tsui112@outlook.com>
2020-09-14 17:57:59 -0700

    gettig bearer token back

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import requests
 import base64
+import sys
 app = Flask(__name__)
 
 app_authentication_data = {
@@ -13,21 +14,26 @@ app_authentication_data = {
     },
     "authorization_data": {
     "grant_type": "client_credentials"
-    }
+    },
+    "bearer_token":""
 }
 def generate_base64_key():
-    user_authorization = f"{app_authentication_data['api_key']:{app_authentication_data['api_secret']}}".encode("ascii")
+    user_authorization = "{}:{}".format(app_authentication_data['api_key'], app_authentication_data['api_secret']).encode("ascii")
     base64_key = base64.standard_b64encode(user_authorization)
     base64_key = base64_key.decode("ascii")
     app_authentication_data["authorization_headers"]["Authorization"] = f"Basic {base64_key}"
 
 def post_request_twitter_API():
-    response_object = requests.post(
+    response = requests.post(
     app_authentication_data["bearer_token_URL"],
     headers=app_authentication_data["authorization_headers"],
     data=app_authentication_data["authorization_data"])
-    )
+    app_authentication_data["bearer_token"] = response.json()["access_token"]
 
+generate_base64_key()
+post_request_twitter_API()
+print(f"{app_authentication_data['bearer_token']}", file=sys.stderr)
+print('This is error output', file=sys.stderr)
 
 @app.route('/')
 def home():
