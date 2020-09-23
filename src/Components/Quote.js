e00a83bccfa5ae6d535bdeed1e226b51db5bc5e0
Chris Tsui <chris.tsui112@outlook.com>
2020-09-23 08:38:20 -0700

    request

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -6,7 +6,7 @@ function Quote(props) {
       <div className="container">
         <blockquote className="blockquote py-5 text-center border-0">
           <p className="mb-0 display-4">{props.tweet.full_text}</p>
-          <footer className="blockquote-footer "></footer>
+          <footer className="blockquote-footer ">{props.tweet.user.name}</footer>
         </blockquote>
       </div>
     </section>
